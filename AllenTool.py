6e913c34779967d3e2731b96c14b84f6117aa3d8
rrtucci <tucci@ar-tiste.com>
2023-09-15 19:06:06 -0400

    initial commit

--- AllenTool.py
+++ AllenTool.py
@@ -6,28 +6,73 @@ from SaxExtraction import *
 
 class AllenTool:
     """
-    
     Hercule Poirot is a fictional Belgian detective , created by Agatha Christie .	<arg1> Hercule Poirot </arg1> <rel> is </rel> <arg2> a fictional Belgian detective , created by Agatha Christie </arg2>	0.95
     Hercule Poirot is a fictional Belgian detective , created by Agatha Christie .	<arg1> Hercule Poirot </arg1> <rel> is </rel> <arg2> a fictional Belgian detective </arg2>	-108.0506591796875
     Hercule Poirot is a fictional Belgian detective , created by Agatha Christie .	<arg1> a fictional Belgian detective </arg1> <rel> created </rel> <arg2> by Agatha Christie </arg2>	0.92
     Hercule Poirot is a fictional Belgian detective , created by Agatha Christie .	<arg1> a fictional Belgian detective </arg1> <rel> be created </rel> <arg2> by Agatha Christie </arg2>	-108.0506591796875
     Hercule Poirot is a fictional Belgian detective , created by Agatha Christie .	<arg1> Hercule Poirot </arg1> <rel> is </rel> <arg2> a fictional Belgian detective created by Agatha Christie </arg2>	-108.0506591796875
 
-    No blank lines between examples.
-    All lines start with original sent, without unused string.
+    This class is for extracting info from an "AllenNLP" file, or "Allen"
+    file, for short. Above are 5 lines of an Allen file. They have the same
+    original sentences (the first sentence of each line). Hence, they all
+    refer to the same "sample". The five lines differ in what comes after
+    the original sentence ("osent"). What comes after the osent in each line
+    is an "extraction". In Allen files, an extraction is composed of 3 parts:
+
+    argument1, demarcated by `<arg1></arg1>`
+    relationship, demarcated by `<rel></rel>`
+    argument2, demarcated by `<arg2></arg2>`
+
+    No blank lines between samples.
+
+    osentL = osent + " [ unused1] [unused2] [unused3]"
+    An `osent` plus the UNUSUSED_TOKEN_STR will be denoted by `osentL`,
+    where the "L" stands for "Long"
+
+    Attributes
+    ----------
+    allen_fp: str
+        Allen file path
+    num_sents: int
+        number of sentences
+    sentL_to_exs: dict[str,list[SaxExtraction]]
+        sentenceLong to extraction list mapping
     
     """
 
     def __init__(self, allen_fp):
+        """
+        Constructor. Note that it automatically reads the Allen file with
+        path `allen_fp` and stores its information in the attribute
+        `self.sentL_to_exs`.
+
+        Parameters
+        ----------
+        allen_fp: str
+        """
         self.allen_fp = allen_fp
         # ex =extraction sent=sentence
         self.sentL_to_exs = self.read_allen_file()
-        #print("mklop", self.sentL_to_exs)
+        # print("mklop", self.sentL_to_exs)
         self.num_sents = len(self.sentL_to_exs)
         # print("lkop", self.num_sents)
 
     @staticmethod
     def get_ex_from_allen_line(line):
+        """
+        This method takes as input a single line `line` from an Allen file
+        and returns an object of the class SaxExtraction. (Sax stands for
+        Sentence Ax, the name of this app).
+
+        Parameters
+        ----------
+        line: str
+
+        Returns
+        -------
+        SaxExtraction
+
+        """
         tab_sep_vals = line.strip().split('\t')
         in_sent = tab_sep_vals[0]
         score = float(tab_sep_vals[2])
@@ -54,14 +99,39 @@ class AllenTool:
 
     @staticmethod
     def get_allen_line_from_ex(ex):
+        """
+        Thiis method takes as input an object `ex` of the class
+        `SaxExtraction` and it returns a string formatted as a line of an
+        Allen file. Hence, this method is the inverse of the method
+        `get_ex_from_allen_line()`.
+
+        Parameters
+        ----------
+        ex: SaxExtraction
+
+        Returns
+        -------
+        str
+
+        """
         str0 = ex.orig_sentL + "\t"
         str0 += " <arg1> " + ex.arg1 + r" <\arg1> "
-        str0 += " <rel> " + ex.pred + r" <\rel> "
+        str0 += " <rel> " + ex.rel + r" <\rel> "
         str0 += " <arg2> " + ex.arg2 + r" <\arg2> \t"
         str0 += str(ex.score)
         return str0
 
     def read_allen_file(self):
+        """
+        This method reads the file with file path `self.allen_fp` and it
+        outputs that file's info as the dictionary `senL_to_exs`. This
+        method is called by the class constructor.
+
+        Returns
+        -------
+        dict[str, list[SaxExtraction]]
+
+        """
         with open(self.allen_fp, 'r', encoding='utf-8') as f:
             lines = f.readlines()
         lines = [unidecode(line) for line in lines]
@@ -71,7 +141,7 @@ class AllenTool:
         for line in lines:
             # print("bnhk", line)
             ex = AllenTool.get_ex_from_allen_line(line)
-            if (prev_in_sentL and ex.orig_sentL != prev_in_sentL) :
+            if prev_in_sentL and ex.orig_sentL != prev_in_sentL:
                 sentL_to_exs[prev_in_sentL] = exs
                 exs = []
             exs.append(ex)
@@ -87,21 +157,50 @@ class AllenTool:
                                      last_sample_id=-1,
                                      ftype="ex",
                                      numbered=False):
+
         """
-        * extags (openie-data\openie4_labels)
-        2.
         Hercule Poirot is a fictional Belgian detective , created by Agatha Christie . [unused1] [unused2] [unused3]
         ARG1 ARG1 REL ARG2 ARG2 ARG2 ARG2 ARG2 ARG2 ARG2 ARG2 ARG2 NONE NONE NONE NONE
         NONE NONE NONE ARG1 ARG1 ARG1 ARG1 NONE REL ARG2 ARG2 ARG2 NONE NONE NONE NONE
-        
-        sample id 1-based
-        
+
+        Above 3 lines come from an "extags file". Extags are extraction tags
+        from the set set('NONE', 'ARG1', 'REL', 'ARG2', 'LOC', 'TIME',
+        'TYPE', 'ARGS'). These 3 lines represent a single "sample". The
+        first line is osentL, the next 2 lines give the extags of
+        extractions from that osent. The osent line and the 2 extraction
+        lines have the same number of words. (You can get the words of a
+        text string using sax_utils.get_words()).
+
+        This method writes an extags file (if ftype=="ex") or a simple
+        sentences file (if ftype=="ss"). In a simple sentences file,
+        the extractions are represented by the osent words of the extags
+        which are different from NONE. We refer to "ex" and "ss" files as
+        "alternatives" to an Allen file.
+
+        If `numbered` is set to False, the output file will have no blank
+        lines separating samples. If it is set to True, it will have a line
+        with a number and period at the beginning of each sample.
+
+
         Parameters
         ----------
-
+        out_fp: str
+            output file path
+        first_sample_id: int
+            number of first sample to be written in output file (1-based)
+        last_sample_id: int
+            number of last sample to be written in output file (1-based)
+        ftype: str
+            ftype in ["ex", "ss"]. If ftype=="ex", an extags file is
+            written. If ftype=="ss", a simple sentences file is written
+            instead.
+        numbered:
+            True iff a 1-based number label will be written as the first
+            line of each sample.
 
         Returns
         -------
+        None
 
         """
 
@@ -113,48 +212,45 @@ class AllenTool:
             num_sams = 0
             for sent, l_ex in self.sentL_to_exs.items():
                 sample_id += 1
-                if sample_id < first_sample_id:
-                    continue
-                if last_sample_id>0 and sample_id> last_sample_id:
+                if sample_id < first_sample_id or sample_id > last_sample_id:
                     continue
                 if numbered:
                     f.write(str(sample_id) + ".\n")
                 num_sams += 1
                 f.write(sent + "\n")
                 for ex in l_ex:
-                    if ftype == "ex": # extags file
+                    if ftype == "ex":  # extags file
                         ex.set_extags()
-                        f.write(" ".join(ex.extags)+ "\n")
-                    elif ftype == "ss": # simple sentences file
+                        f.write(" ".join(ex.extags) + "\n")
+                    elif ftype == "ss":  # simple sentences file
                         f.write(ex.get_simple_sent() + "\n")
                     else:
                         assert False
-            print("finished file " + out_fp + ":    "+
+            print("finished file " + out_fp + ":    " +
                   str(num_sams) + " samples.")
-
-                # if ex.name_is_tagged["ARG1"] and \
-                #         ex.name_is_tagged["REL"] and \
-                #         ex.name_is_tagged["ARG2"]:
-                #     if 'ARG1' in ex.sent_tags and 'REL' in ex.sent_tags:
-                #         if (not ex.arg2) or 'ARG2' in ex.sent_tags:
-                #             f.write(ex.orig_sentL + "\n")
-                #             if new_in_sent:
-                #                 f.write(' '.join(ex.in3_tokens))
-                #                 f.write('\n')
-                #             f.write(' '.join(ex.sent_tags))
-                #             f.write('\n')
 
     def write_extags_ttt_files(self,
                                out_dir,
                                ttt_fractions=(.6, .2, .2)):
         """
-        ttt = train, val, test
+        ttt = train, dev, test
         tuning=dev=development=validation
+
+        This method uses as input the samples loaded by the constructor from
+        an Allen file. The method outputs 3 files into the directory
+        `out_dir`. Those 3 files will be used for training, development and
+        testing, respectively.
+
+
 
         Parameters
         ----------
-        out_dir
-        ttt_fractions
+        out_dir: str
+            Output directory
+        ttt_fractions: list[float, float, float]
+            The 3 fractions [f_train, f_dev, f_test] should add to one. They
+            indicate the fraction of the samples from the input Allen file
+            that will be devoted to training, development and testing.
 
         Returns
         -------
@@ -167,10 +263,10 @@ class AllenTool:
             1,
             num_train_sents)
         val_first_last = (
-            num_train_sents+1,
+            num_train_sents + 1,
             num_train_sents + num_val_sents)
         test_first_last = (
-            num_train_sents + num_val_sents+1,
+            num_train_sents + num_val_sents + 1,
             num_train_sents + num_val_sents + num_test_sents)
 
         # print("ghjy", train_first_last, val_first_last, test_first_last)
@@ -187,9 +283,9 @@ class AllenTool:
 if __name__ == "__main__":
     def main1(ftype):
         allen_fp = "testing_files/small_allen.tsv"
-        if ftype=="ex":
+        if ftype == "ex":
             out_fp = "testing_files/small_extags.txt"
-        elif ftype=="ss":
+        elif ftype == "ss":
             out_fp = "testing_files/small_simple_sents.txt"
         else:
             assert False
@@ -199,14 +295,15 @@ if __name__ == "__main__":
                                         last_sample_id=-1,
                                         ftype=ftype,
                                         numbered=True)
+
+
     def main2():
         allen_fp = "testing_files/small_allen.tsv"
-        out_dir = "testing_files/out"
+        out_dir = "testing_files"
         at = AllenTool(allen_fp)
         at.write_extags_ttt_files(out_dir)
 
 
-
-    # main1("ss")
+    main1("ss")
     main1("ex")
-    #main2()
+    main2()
